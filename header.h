8de393dc4f4eb2be4bf39ba09f857c84d7124176
Mihalik-Matej <82832320+Mihalik-Matej@users.noreply.github.com>
2021-12-08 14:33:00 +0100

    Add files via upload

--- header.h
+++ header.h
@@ -1,3 +1,4 @@
+
 #include <iostream>
 #include <string>
 #include <vector>
@@ -7,9 +8,10 @@ const int x = 211;
 const int y = 49;
 string screen[y][x];
 bool game = true;
-string RiadkovyText= "";
+string RiadkovyText = "";
 //bullety
 vector<int> BulletL;
+char close = ' ';
 
 
 void clear()
@@ -20,66 +22,66 @@ void clear()
 
 void draw()
 {
-    for (int i = 0; i < y ; i++)
+    for (int i = 0; i < y; i++)
 
     {
-        for (int j = 0 ; j < x ; j++)
+        for (int j = 0; j < x; j++)
         {
             RiadkovyText += screen[i][j];
         }
-    cout <<RiadkovyText<< endl;
-    RiadkovyText = "";
+        cout << RiadkovyText << endl;
+        RiadkovyText = "";
     }
-    
+
 
 }
 void pozadie()
 {
-     for (int i = 0; i < y ; i++)
+    for (int i = 0; i < y; i++)
     {
-        for (int j = 0 ; j < x ; j++)
+        for (int j = 0; j < x; j++)
         {
-            if (i == 0 )
+            if (i == 0)
             {
                 if (j == 0)
                 {
-                    screen[i][j] = "┌";
+                    screen[i][j] = "\xDA";
                     continue;
                 }
-                if (j == x-1)
+                if (j == x - 1)
                 {
-                    screen[i][j] = "┐";
+                    screen[i][j] = "\xBF";
                     continue;
                 }
-                screen[i][j] = "─";
+                screen[i][j] = "\xC4";
             }
-            else if (i == y-1)
+            else if (i == y - 1)
             {
                 if (j == 0)
                 {
-                    screen[i][j] = "└";
+                    screen[i][j] = "\xC1";
                     continue;
                 }
-                
-                if (j == x-1)
+
+                if (j == x - 1)
                 {
-                    screen[i][j] = "┘";
+                    screen[i][j] = "\xD9";
                     continue;
                 }
-                screen[i][j] = "─";
+                screen[i][j] = "\xC4";
             }
-            else if (j == 0 )
+            else if (j == 0)
             {
-                
-                screen[i][j] = '|';
+
+                screen[i][j] = '\xB3';
             }
-            else if (j == x-1)
+            else if (j == x - 1)
             {
-                screen[i][j] = '|';
+                screen[i][j] = '\xB3';
             }
             else
             {
-            screen[i][j] = ' ';
+                screen[i][j] = ' ';
             }
         }
     }
@@ -89,48 +91,46 @@ void pozadie()
 void playerDraw(int x, int y, int W, int H, string rotation)
 {
 
-        for (int i = 0 ; i < H ; i++)
+    for (int i = 0; i < H; i++)
+    {
+        for (int j = 0; j < W; j++)
         {
-            for (int j = 0 ; j < W ; j++)
+            if (i == 0)
             {
-                if (i == 0)
+                if (j == 0 || j == W - 1)
                 {
-                    if (j == 0 || j == W - 1)
-                    {
-                        screen[y+i][x+j] = "▄";
-                        continue;
-                    }
+                    screen[y + i][x + j] = "\xDC";
+                    continue;
                 }
-                if (i == H-1)
-                {
-                    if (j == 0 || j == W - 1)
-                    {
-                        screen[y+i][x+j] = "▀";
-                        continue;
-                    }
-                }
-                screen[y+i][x+j] = "█";
             }
+            if (i == H - 1)
+            {
+                if (j == 0 || j == W - 1)
+                {
+                    screen[y + i][x + j] = "\xDF";
+                    continue;
+                }
+            }
+            screen[y + i][x + j] = "\xDB";
         }
-        if (rotation == "vpravo")
-        {
-            screen[y + (H/2)][x + W] = "─"; 
-            screen[y + (H/2)][x + W + 1] = "─"; 
-        }
-        if (rotation == "vlavo")
-        {
-            screen[y + (H/2)][x - 1] = "─"; 
-            screen[y + (H/2)][x - 2] = "─"; 
-        }
-        if (rotation == "hore")
-        {
-            screen[y - 1][x + H - 1] = "|";   
-        }
-        if (rotation == "dole")
-        {
-            screen[y + H][x + H - 1] = "|";   
-        }
+    }
+    if (rotation == "vpravo")
+    {
+        screen[y + (H / 2)][x + W] = "\xC4";
+        screen[y + (H / 2)][x + W + 1] = "\xC4";
+    }
+    if (rotation == "vlavo")
+    {
+        screen[y + (H / 2)][x - 1] = "\xC4";
+        screen[y + (H / 2)][x - 2] = "\xC4";
+    }
+    if (rotation == "hore")
+    {
+        screen[y - 1][x + H - 1] = "\xB3";
+    }
+    if (rotation == "dole")
+    {
+        screen[y + H][x + H - 1] = "\xB3";
+    }
 }
-
-
 
--- main.cpp
+++ main.cpp
@@ -3,24 +3,25 @@
 #include <string>
 #include <vector>
 #include "header.h"
+#include <ctype.h>
 using namespace std;
 
 
 int main() {
 
-    
+
 
     class player {
-        public:
-            int x = 105;
-            int y = 25;
-            int width = 5;
-            int height = 3;
-            
-            int speedY = 1;
-            int speedX = 2;
+    public:
+        int x = 105;
+        int y = 25;
+        int width = 5;
+        int height = 3;
 
-            string rotation = "vpravo";
+        int speedY = 1;
+        int speedX = 2;
+
+        string rotation = "vpravo";
     };
     player player;
 
@@ -29,93 +30,104 @@ int main() {
 
 
     class bullet {
-        public:
-            int x;
-            int y;
-            char shape = '.';
+    public:
+        int x;
+        int y;
+        char shape = '.';
     };
+
+    SetConsoleCP(437);
+    SetConsoleOutputCP(437);
+
+
     bullet Bullet;
     while (game)
     {
-        
-        
-        
-        
 
 
-        if(GetKeyState('D') & 0x8000)
-            {  
-                if (player.x < x - player.speedX - player.width - 1) 
-                    {
-                    player.x += player.speedX;
-                    }
-            }
-        if(GetKeyState('A') & 0x8000)
-            {   
-                if (player.x > 0 + player.speedX + 1) 
-                    {
-                    player.x -= player.speedX;
-                    }
 
+
+
+
+        if (GetKeyState('D') & 0x8000)
+        {
+            if (player.x < x - player.speedX - player.width - 1)
+            {
+                player.x += player.speedX;
             }
-        if(GetKeyState('W') & 0x8000)
-            {   
-                if (player.y > 0 + player.speedY + 1) 
-                    {
-                    player.y -= player.speedY;
-                    }
+        }
+        if (GetKeyState('A') & 0x8000)
+        {
+            if (player.x > 0 + player.speedX + 1)
+            {
+                player.x -= player.speedX;
             }
-        if(GetKeyState('S') & 0x8000)
-            {   
-                if (player.y < y-player.speedY-player.height - 1) 
-                    {
-                    player.y += player.speedY;
-                    }
+
+        }
+        if (GetKeyState('W') & 0x8000)
+        {
+            if (player.y > 0 + player.speedY + 1)
+            {
+                player.y -= player.speedY;
             }
+        }
+        if (GetKeyState('S') & 0x8000)
+        {
+            if (player.y < y - player.speedY - player.height - 1)
+            {
+                player.y += player.speedY;
+            }
+        }
         //-----------rotacia-----------
-        if(GetKeyState('L') & 0x8000)
+        if (GetKeyState('L') & 0x8000)
         {
             player.rotation = "vpravo";
         }
-        else if(GetKeyState('J') & 0x8000)
+        else if (GetKeyState('J') & 0x8000)
         {
             player.rotation = "vlavo";
         }
-        else if(GetKeyState('I') & 0x8000)
+        else if (GetKeyState('I') & 0x8000)
         {
             player.rotation = "hore";
         }
-        else if(GetKeyState('K') & 0x8000)
+        else if (GetKeyState('K') & 0x8000)
         {
             player.rotation = "dole";
         }
         if (GetKeyState(32) & 0x8000) // t.j. medzerník
 
         {
-            
+
+        }
+
+        if (GetKeyState(27) & 0x8000) // esc
+        {
+            game = false;
+            cout << "close?(Y/N)";
+            cin >> close;
+            close = ((char)toupper(close));
+            if (close == 'Y')
+                break;
+            else
+                game = true;
         }
 
 
-
-
-
         pozadie();
-        
+
         //zatial rucne spraveny character
-        playerDraw(player.x,player.y,player.width,player.height, player.rotation);
-        
+        playerDraw(player.x, player.y, player.width, player.height, player.rotation);
+
 
         clear();
-        cout <<" Health: 100                  "<< "X:" << player.x <<"    Y:" << player.y << "     rotation: " << player.rotation << endl;
+        cout << " Health: 100                  " << "X:" << player.x << "    Y:" << player.y << "     rotation: " << player.rotation << endl;
         draw();
-        
 
-        
-        
+
     }
-    
 
+    clear();
     
-    system("PAUSE");
     return 0;
 }
